4bfdadc057428d17acc50bff027cd56f11105d6e
Heon Soo Park <hspark220@gmail.com>
2023-03-03 13:11:48 -0500

    Add style Add icon Add ui for getting temp

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const weather = (() => {
         try {
             const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},US&APPID=fe326985646cdbc738b7348445ec490c&units=metric`, {mode: 'cors'});
             const weatherData = await response.json();
-            console.log(weatherData);
+            return weatherData;
         } catch {
             console.err('Error retrieving weather...')
         }
@@ -14,5 +14,63 @@ const weather = (() => {
     return {getWeather}
 })();
 
-weather.getWeather('toledo');
+const ui = (() => {
 
+    const _getData = async () => {
+        const city = document.getElementById('city');
+        const temperature = document.getElementById('temperature');
+        const title = document.querySelector('h1');
+        const icon = document.querySelector('#img');
+        const data =  await weather.getWeather(city.value);
+        title.innerHTML = `${data.name}`
+        temperature.innerHTML = `${data.main.temp}°C`;
+        const id = await data.weather[0].icon
+        icon.setAttribute('src', `http://openweathermap.org/img/wn/${id}@2x.png`);
+
+         //_printInfos(data);
+    }
+
+    const _enterSubmit = e => {
+        if (e.key === "Enter") {
+            _getData();
+        }
+    }
+
+    const _printInfos = data => {
+        try {
+            const infos = document.querySelector('.infos');
+            infos.remove();
+        } catch {}
+
+        const infos = document.createElement('div');
+        infos.setAttribute('class','infos');
+        document.body.append(infos);
+
+        Object.entries(data.main).forEach(info => {
+            const p = document.createElement('p');
+            p.setAttribute('class','info');
+            p.setAttribute('class', info[0]);
+            p.innerHTML = `${info[0]}: ${info[1]}`
+            infos.append(p);
+        });
+    }
+
+    const setup = () => {
+        const button = document.getElementById('submit');
+        const city = document.getElementById('city');
+        const form = document.querySelector('form');
+
+        form.addEventListener('keydown', _enterSubmit);
+        button.addEventListener('click', _getData);
+
+        city.focus();
+        
+
+
+
+    }
+
+    return {setup}
+})();
+
+ui.setup();
